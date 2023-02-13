9cf53ce323fca75c31516fded65a5fc2ea9614a3
a_yaroslavtseva <a_yaroslavtseva@eureca.ru>
2023-02-13 10:11:29 +0300

    signal handling

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,33 @@
 #include "textedit.h"
 #include "localserver.h"
 
+#include <signal.h>
+
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include <QScreen>
+
+void catchUnixSignals(std::initializer_list<int> quitSignals) {
+    auto handler = [](int sig) -> void {
+        // blocking and not aysnc-signal-safe func are valid
+        printf("\nquit the application by signal(%d).\n", sig);
+        QCoreApplication::quit();
+    };
+
+    sigset_t blocking_mask;
+    sigemptyset(&blocking_mask);
+    for (auto sig : quitSignals)
+        sigaddset(&blocking_mask, sig);
+
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sa.sa_mask    = blocking_mask;
+    sa.sa_flags   = 0;
+
+    for (auto sig : quitSignals)
+        sigaction(sig, &sa, nullptr);
+}
 
 
 int main(int argc, char *argv[])
@@ -62,6 +85,7 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(textedit);
 
     QApplication a(argc, argv);
+    catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
 
     QCoreApplication::setOrganizationName("QtProject");
     QCoreApplication::setApplicationName("Rich Text");
